556efdbfdf637707fcad7fb96a00a187d5de04f2
Federico Interlandi Zoireff <ing.finterlandi@gmail.com>
2023-06-14 21:01:33 -0300

    404 response

--- src/routes/autoCheckIn.ts
+++ src/routes/autoCheckIn.ts
@@ -7,6 +7,7 @@ export default function checkIn(): Router {
     return Router()
         .get("/:id/passengers", async (req: Request, res:Response, next: NextFunction) => {
             const result = await autoCheckIn(req.params.id);
+            if(!result.length) return res.status(StatusCodes.NOT_FOUND).send({code: StatusCodes.NOT_FOUND, data: {}})
             return res.send({code: StatusCodes.OK, data: result})
         })
     
