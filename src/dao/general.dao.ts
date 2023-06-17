684d7ad82367c7e7c990057612e85b4cdf090ea9
Federico Interlandi Zoireff <ing.finterlandi@gmail.com>
2023-06-16 22:07:34 -0300

    sillas libres ordenadas por cercania

--- src/dao/general.dao.ts
+++ src/dao/general.dao.ts
@@ -3,37 +3,104 @@ import { getConnection } from "../db";
 
 import { Passenger } from "../domain/passenger";
 import { GeneralRepo } from "../repositories/general.repo";
-import { BoardingPass } from "../domain/boardingPass";
 import { Flight } from "../domain/flight";
+import { Seat } from "../domain/seat";
 
-export class GeneralDAO{
+export class GeneralDAO implements GeneralRepo{
 
     async readFlight(id: string | undefined): Promise<Flight> {
         let sql = `SELECT * FROM flight WHERE flight.flight_id = ${id}`;
         const conn = await getConnection();
         const result = await conn.manager.query(sql);
-        return result[0] as Flight;
+        const flight: Flight = {
+            flightId: result[0]['flight_id'],
+            takeoffDateTime: result[0]['takeoff_date_time'],
+            takeoffAirport: result[0]['takeoff_airport'],
+            landingDateTime: result[0]['landing_date_time'],
+            landingAirport: result[0]['landing_airport'],
+            airplaneId: result[0]['airplane_id']
+        }
+        return flight as Flight;
     }
 
-    async searchPassenger(query?: string | undefined): Promise<Passenger[]> {
-        let sql = `SELECT * FROM passenger`;
-        if(query){
-            sql = query;
-        }
+    async searchPassengerByFlight(id: string): Promise<Passenger[]> {
+        let sql = `SELECT p.passenger_id, p.dni, p.name, p.age, p.country, bp.boarding_pass_id, bp.purchase_id, bp.seat_type_id, bp.seat_id
+        FROM passenger p 
+        JOIN boarding_pass bp ON p.passenger_id = bp.passenger_id 
+        WHERE bp.flight_id = ${id}
+        ORDER BY bp.seat_type_id, bp.purchase_id`;
+        
         const conn = await getConnection();
         const result = await conn.manager.query(sql);
-        return result as Passenger[];
+        const passenger : Passenger[] = [];
+        for(let r of result){
+            let p: Passenger = {
+                passengerId: r['passenger_id'],
+                dni: parseInt(r['dni']),
+                name: r['name'],
+                age: r['age'],
+                country: r['country'],
+                boardingPassId: r['boarding_pass_id'],
+                purchaseId: r['purchase_id'],
+                seatTypeId: r['seat_type_id'],
+                seatId: r['seat_id']
+            }
+            passenger.push(p)
+        }
+        return passenger as Passenger[];
     }
   
-    async readSeats(id: number){
-        let sql = `SELECT seat_id, seat_column, seat_row, seat_type_id 
-                    FROM airplane a JOIN seat s 
-                    ON a.airplane_id = s.airplane_id 
-                    WHERE a.airplane_id = ${id}
-                    ORDER BY s.seat_row ASC, s.seat_column ASC`
-
+    async readFreeSeats(passengers: Passenger[], id: string) {
+        let sql = `SELECT s.seat_id, s.seat_column, s.seat_row, s.seat_type_id
+                    FROM seat s 
+                    JOIN airplane a ON s.airplane_id = a.airplane_id
+                    JOIN flight f ON f.airplane_id = a.airplane_id               
+                    WHERE f.flight_id = ${id}
+                    ORDER BY s.seat_row, s.seat_column;`;
+        
         const conn = await getConnection();
         const result = await conn.manager.query(sql);
-        return result;
+        
+        const seats : Seat[] = [];
+
+    //ASIENTOS OCUPADOS
+     const fullSeats = passengers.map(p => p.seatId);
+
+    //ASIENTOS LIBRES
+     //const freeSeats = result.filter(r => !fullSeats.includes(r['seat_id']));
+        for(let r of result){
+            let p: Seat = {
+                seatId: r['seat_id'],
+                seatColumn: r['seat_column'],
+                seatRow: r['seat_row'],
+                seatTypeId: r['seat_type_id'],
+                airplaneId: r['airplane_id']
+            }
+            seats.push(p)
+        }
+        return ordenarAsientosCercania(seats).filter(s => !fullSeats.includes(s.seatId)) as Seat[];
+        return seats as Seat[];
     }
+}
+
+function calculateDistance(seat1: Seat, seat2: Seat): number {
+    const distance = Math.abs(seat1.seatColumn.charCodeAt(0) - seat2.seatColumn.charCodeAt(0)) + Math.abs(seat1.seatRow - seat2.seatRow);
+    return distance;
+}
+  
+function ordenarAsientosCercania(asientos: Seat[]): Seat[] {
+// Crear una copia del array original
+const sortedAsientos: Seat[] = [...asientos];
+
+// Obtener el primer asiento (puede ser aleatorio)
+const currentSeat = sortedAsientos[0];
+
+// Ordenar los asientos restantes basado en la distancia al asiento actual
+sortedAsientos.sort((seat1, seat2) => {
+    const distance1 = calculateDistance(currentSeat, seat1);
+    const distance2 = calculateDistance(currentSeat, seat2);
+    return distance1 - distance2;
+});
+
+return sortedAsientos;
 }
